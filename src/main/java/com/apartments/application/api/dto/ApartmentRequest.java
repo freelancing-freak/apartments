8498bf658ba2e18ae234423029b5c4cb20b886df
Robert Krzywina <robert.krzywina@gmail.com>
2022-10-12 14:41:20 +0200

    Change @NotEmpty to @NotNull

--- src/main/java/com/apartments/application/api/dto/ApartmentRequest.java
+++ src/main/java/com/apartments/application/api/dto/ApartmentRequest.java
@@ -1,15 +1,15 @@
 package com.apartments.application.api.dto;
 
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.Set;
 
-public record ApartmentRequest(@NotEmpty String name,
-                               @NotEmpty String description,
-                               @NotEmpty String location,
-                               @NotEmpty String locationSrc,
-                               @NotEmpty int measurement,
-                               @NotEmpty int numberOfRooms,
-                               @NotEmpty String price,
-                               @NotEmpty String mainImage,
-                               @NotEmpty Set<String> images) {
+public record ApartmentRequest(@NotNull String name,
+                               @NotNull String description,
+                               @NotNull String location,
+                               @NotNull String locationSrc,
+                               @NotNull int measurement,
+                               @NotNull int numberOfRooms,
+                               @NotNull String price,
+                               @NotNull String mainImage,
+                               @NotNull Set<String> images) {
 }
