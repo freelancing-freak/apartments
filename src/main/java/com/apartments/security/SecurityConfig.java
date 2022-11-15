27ee43561ad5ddc7417dac6147b6dd6fb002aeea
Robert Krzywina <robert.krzywina@gmail.com>
2022-11-15 20:37:42 +0100

    Permit all api endpoints

--- src/main/java/com/apartments/security/SecurityConfig.java
+++ src/main/java/com/apartments/security/SecurityConfig.java
@@ -25,8 +25,7 @@ public class SecurityConfig extends VaadinWebSecurity {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // Authorize access to /images/ without authentication
-        http.authorizeRequests().antMatchers("/images/**").permitAll();
+        http.authorizeRequests().antMatchers("/images/**", "/api/**").permitAll();
         // Set default security policy that permits Vaadin internal requests and
         // denies all other
         super.configure(http);
